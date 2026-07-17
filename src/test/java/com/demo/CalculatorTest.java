b2063e19c40de21794881a69fe47f2028b37430a
Ansh Matlotia <anshmatlotia@gmail.com>
2026-07-17 16:48:11 +0530

    Initial commit

---
+++ src/test/java/com/demo/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+}
